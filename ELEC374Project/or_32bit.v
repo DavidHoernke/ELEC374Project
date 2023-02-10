a113d86d68cc0fd5f5414e2e4730fa016d24f2be
DavidHoernke <david.hoernke23@gmail.com>
2023-02-10 15:12:20 -0500

    fixed or

--- ELEC374Project/or_32bit.v
+++ ELEC374Project/or_32bit.v
@@ -9,9 +9,9 @@ module or_32bit(
         for(i=0;i<32;i=i+1) begin : loop
         // check with TA
             if (Ra[i]+Rb[i]>0)
-                Rz[i]<=1;
+                Rz[i]=1;
             else
-                Rz[i]<=0;
+                Rz[i]=0;
         end
     endgenerate
 endmodule
